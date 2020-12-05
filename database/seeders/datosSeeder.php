e9080c253e73b08b4226be9931d3e53741556449
Mayra-Salazar <l16181180@slp.tecnm.mx>
2020-12-05 15:00:54 -0600

    Base de datos

---
+++ database/seeders/datosSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class datosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('datos')->insert([
+          'nombre'=> 'Estructura de datos',
+          'autor' => 'Luis Joyanes',
+      ]);
+
+      DB::table('datos')->insert([
+          'nombre'=> 'Prog. Orientada a Objetos',
+          'autor' => 'Luis Joyanes'.
+      ]);
+    }
+}
